30f5bfc9144bb3a1c5f24f69c269a6e6f6aff6db
Nader <abolfazli.nader@gmail.com>
2017-11-11 12:12:54 +0330

    rading inputs

--- src/ir/maktab/Main.java
+++ src/ir/maktab/Main.java
@@ -1,8 +1,18 @@
 package ir.maktab;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        String a;
+        int[][] arr= new int[3][3];
+        for (int i=0; i<3; i++)
+            for (int j=0; j<3; j++) {
+                a = in.next();
+                arr[i][j] = a.equals("x") ? 0 : Integer.parseInt(a);
+            }
     }
 }
