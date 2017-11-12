1b50a4e76ab067270f427a78db0a35e4439bc126
NaderAbolfazli <abolfazli.nader@gmail.com>
2017-11-12 15:42:25 +0330

    final (3*3 table)

--- src/ir/maktab/Main.java
+++ src/ir/maktab/Main.java
@@ -1,6 +1,5 @@
 package ir.maktab;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        Random rand = new Random(3);
         String a;
         int[][] cells = new int[3][3];
         for (int i = 0; i < 3; i++)
@@ -17,28 +15,22 @@ public class Main {
                 cells[i][j] = a.equals("x") ? 0 : Integer.parseInt(a);
             }
 
-        //for (int i=0; i<3; i++)
-        //for (int j=0; j<3; j++){
-        //if (arr[i][j]==0){
-        int soulutionCount = 0;
-        solve(0, 0, cells, soulutionCount);
+        System.out.print("solution Number=" + solve(0, 0, cells, 0));
     }
 
     // returns 0, 1 or more than 1 depending on whether 0, 1 or more than 1 solutions are found
     static int solve(int i, int j, int[][] cells, int count /*initailly called with 0*/) {
-        if (i == 9) {
+        if (i == 3) {
             i = 0;
-            if (++j == 9)
+            if (++j == 3)
                 return 1 + count;
         }
         if (cells[i][j] != 0)  // skip filled cells
             return solve(i + 1, j, cells, count);
-        // search for 2 solutions instead of 1
-        // break, if 2 solutions are found
-        for (int val = 1; val <= 9 && count < 2; ++val) {
+        // search for n solutions instead of 1
+        for (int val = 1; val <= 3; ++val) {
             if (legal(i, j, val, cells)) {
                 cells[i][j] = val;
-                // add additional solutions
                 count = solve(i + 1, j, cells, count);
             }
         }
@@ -47,6 +39,10 @@ public class Main {
     }
 
     private static boolean legal(int i, int j, int val, int[][] cells) {
-        return false;
+        for (int n = 0; n < 3; n++) {
+            if ((cells[i][n] == val && n != j) || (cells[n][j] == val && n != i))
+                return false;
+        }
+        return true;
     }
 }
